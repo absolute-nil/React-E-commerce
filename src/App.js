ea6c22d4b1a88e1448eb2d7e3790e5a40850caf4
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2021-01-27 01:12:58 +0530

    create header component with link to shop and home page with logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component'
 
-//TODO temporary -> remove this
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HatsPage</h1>
-    </div>
-  );
-}
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
